12457d91f018211be80f0fad3153ad1832e15d38
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2023-05-03 10:13:51 +0200

    Add photo, styling

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,14 +1,16 @@
 <template>
-
-<div class="subpage__body">
-  <section>
-  <h2>I am a Front-end Developer</h2>
-  <p>
-    ...with a degree from <a href="https://medieinstitutet.se/utbildningar/front-end-developer/" target="_new">Medieinstitutet</a>.
-    I enjoy both problem solving and design.
-    I also have a degree in biology and have previously worked with identifying trees, whales and counterfeit passports.
-  </p>
-</section>
-</div>
-
+  <div class="subpage__body">
+    <section>
+      <h2>I am a Front-end Developer</h2>
+      <p class="about">
+        <img src="@/assets/rebecka.png" alt="a photo of Rebecka looking sharp in a sidecut and a black shirt"
+          class="rebecka_portrait">    
+        <span>...with a degree from <a href="https://medieinstitutet.se/utbildningar/front-end-developer/"
+          target="_new">Medieinstitutet</a>.
+        I enjoy both problem solving and design.
+        I also have a degree in biology and have previously worked with identifying trees, whales and counterfeit
+        passports.</span>     
+      </p>
+    </section>
+  </div>
 </template>
--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -2,14 +2,14 @@
 <div class="subpage__body">
   <section>
   <h2>Skills</h2>
-  <ul>
+  <ul class="skills_list">
     <li>HTML, CSS, SASS</li>
     <li>Javascript</li>
+    <li>React</li>
+    <li>Vue</li>
     <li>API:s (REST, JSON)</li>
-    <li>Node.js, express</li>
-    <li>Database querying (SQL, NoSQL)</li>
-    <li>React.js</li>
-    <li>Vue.js</li>
+    <li>Node, express</li>
+    <li>Database querying (SQL, NoSQL)</li> 
     <li>Typescript</li>
     <li>Agile methods</li>
     <li>Graphic design</li>
