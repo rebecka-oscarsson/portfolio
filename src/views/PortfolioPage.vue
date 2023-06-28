8c3f733fd9f546daae7914d278dcbe5e16b9ed1b
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2023-06-28 18:36:59 +0200

    Links added to portfolio

--- src/views/PortfolioPage.vue
+++ src/views/PortfolioPage.vue
@@ -3,11 +3,29 @@
     <h2>Portfolio</h2>
     <section class="subpage__section-portfolio">
 
+      <figure><a href="https://63761a7bd03fb9756e1f6634--ornate-cat-06350f.netlify.app/" target="_blank">
+          <div class="overlay">
+            <div class="imageText">Internship project PostNord (Vue, API:s)</div>
+          </div>
+          <img src="@/assets/postnord.png" alt="Map of Gamla Stan with locations of post offices">
+        </a>
+        <figcaption>Internship project PostNord (Vue)</figcaption>
+      </figure>
+
+      <figure><a href="https://glittery-mousse-4ca3b5.netlify.app/" target="_blank">
+          <div class="overlay">
+            <div class="imageText">A site using Headless CMS (React, Strapi)</div>
+          </div>
+          <img src="@/assets/strapi.png" alt="Website with an image of Stockholm">
+        </a>
+        <figcaption>A site using Headless CMS (React, Strapi)</figcaption>
+      </figure>
+
       <figure v-if="!isMobile"><a href="https://rebecka-oscarsson.github.io/deep/" target="_blank">
           <div class="overlay">
             <div class="imageText">Chat (Node, React, socket.io)</div>
           </div>
-          <img src="@/assets/jelly.png" alt="a jellyfish with a speech bubble">
+          <img src="@/assets/jelly.png" alt="Jellyfish with a speech bubble">
         </a>
         <figcaption>Chat (Node, React, socket.io)</figcaption>
       </figure>
@@ -16,16 +34,16 @@
           <div class="overlay">
             <div class="imageText">Chat (Node, socket.io)</div>
           </div>
-          <img src="@/assets/chat.png" alt="two colorful chat bubbles with text in them">
+          <img src="@/assets/chat.png" alt="Two colorful chat bubbles with text in them">
         </a>
         <figcaption>Chat (Node, socket.io)</figcaption>
       </figure>
 
       <figure><a href="https://63761a7bd03fb9756e1f6634--ornate-cat-06350f.netlify.app/" target="_blank">
           <div class="overlay">
-            <div class="imageText">Weirdopedia (React)</div>
+            <div class="imageText">Weirdopedia (React, API)</div>
           </div>
-          <img src="@/assets/weirdopedia.jpg" alt="a deformed-looking polarbear">
+          <img src="@/assets/weirdopedia.jpg" alt="Deformed-looking polarbear">
         </a>
         <figcaption>Weirdopedia (React)</figcaption>
       </figure>
@@ -34,7 +52,7 @@
           <div class="overlay">
             <div class="imageText">Kiwi game (Javascript)</div>
           </div>
-          <img src="@/assets/kiwi.png" alt="a blue kiwibird standing between two palmtrees">
+          <img src="@/assets/kiwi.png" alt="Blue kiwibird standing between two palmtrees">
         </a>
         <figcaption>Kiwi game (Javascript)</figcaption>
       </figure>
@@ -43,42 +61,33 @@
         <div class="overlay">
           <div class="imageText">Kiwi game (Javascript)</div>
         </div>
-        <img src="@/assets/kiwi.png" alt="a blue kiwibird standing between two palmtrees">
+        <img src="@/assets/kiwi.png" alt="Blue kiwibird standing between two palmtrees">
         <figcaption>Sorry, doesn't work on mobile</figcaption>
       </figure>
-
-      <!-- <figure><a href="https://rebecka-oscarsson.github.io/nyhetsbrev/" target="_blank">
-      <div class="overlay">
-        <div class="imageText">Forum (Node.js, MongoDB, crypto.js)</div>
-      </div>
-      <img src="login.png"
-          alt="a login form"></a>
-      <figcaption>Forum (Node.js, MongoDB, crypto.js)</figcaption>
-    </figure> -->
-
+      
       <figure><a href="https://rebecka-oscarsson.github.io/StarWars/" target="_blank">
           <div class="overlay">
             <div class="imageText">Starwars (Javascript, API)</div>
           </div>
-          <img src="@/assets/starwars.jpg" alt="starwars website with loads of info">
+          <img src="@/assets/starwars.jpg" alt="Website containing loads of StarWars trivia">
         </a>
         <figcaption>Starwars (Javascript, API)</figcaption>
       </figure>
 
       <figure><a href="https://rcode21.github.io/" target="_blank">
           <div class="overlay">
-            <div class="imageText">Creatures: art by Arci Taube (Javascript, SVG)</div>
+            <div class="imageText">Creatures: art project by Arci Taube (Javascript, SVG)</div>
           </div>
-          <img src="@/assets/creatures.png" alt="platypus outline made in a drawing program">
+          <img src="@/assets/creatures.png" alt="Platypus drawn with black lines in a drawing program">
         </a>
-        <figcaption>Creatures: art by Arci Taube (Javascript, SVG)</figcaption>
+        <figcaption>Creatures: art project by Arci Taube (Javascript, SVG)</figcaption>
       </figure>
 
       <figure><a href="https://rebecka-oscarsson.github.io/tv-room/" target="_blank">
           <div class="overlay">
             <div class="imageText">Tv-room: videos by me (Css)</div>
           </div>
-          <img src="@/assets/fjurp.jpg" alt="a tv showing a nature program">
+          <img src="@/assets/fjurp.jpg" alt="Tv showing a nature program">
         </a>
         <figcaption>Tv-room (Css, videos by me)</figcaption>
       </figure>
